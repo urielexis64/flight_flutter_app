4c608d5ecb07d8d93b082026af6a23ac916dc182
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-15 23:34:19 -0700

    PopupMenuItems, main text, textfield, ChoiceChips

--- lib/custom_shape_clipper.dart
+++ lib/custom_shape_clipper.dart
@@ -13,13 +13,13 @@ class CustomShapeClipper extends CustomClipper<Path> {
     Offset firstEndPoint = Offset(width * .5, height - 30);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
+
     Offset secondControlPoint = Offset(width * .75, height - 10);
     Offset secondEndPoint = Offset(width, height - 80);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
     path.lineTo(width, height);
-
     path.lineTo(width, 0);
     path.close();
     return path;
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flight_app/constants.dart';
 import 'package:flight_app/custom_shape_clipper.dart';
 import 'package:flight_app/data.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +8,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: HomeScreen());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+        ),
+        home: HomeScreen());
   }
 }
 
@@ -30,54 +37,152 @@ class HomeScreenTop extends StatefulWidget {
 }
 
 class _HomeScreenTopState extends State<HomeScreenTop> {
+  int _selectedLocationIndex = 0;
+  bool isFlightSelected = true;
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Stack(
       children: [
         ClipPath(
           clipper: CustomShapeClipper(),
           child: Container(
-            height: 400,
+            height: size.height * 0.55,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.orangeAccent, Colors.orange])),
+              gradient: LinearGradient(
+                  colors: [Constants.primaryColor, Constants.secondaryColor]),
+            ),
             child: SafeArea(
               child: Column(
                 children: [
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Icon(
                           Icons.location_on,
-                          color: Colors.white,
+                          color: Constants.lightColor,
+                        ),
+                        SizedBox(
+                          width: 10,
                         ),
                         PopupMenuButton(
+                          initialValue: _selectedLocationIndex,
+                          onSelected: (index) {
+                            setState(() {
+                              _selectedLocationIndex = index;
+                            });
+                          },
                           itemBuilder: (BuildContext context) {
                             return <PopupMenuItem<int>>[
                               PopupMenuItem(
-                                  child: Text(locations[0]), value: 0),
+                                  child: Text(
+                                    locations[0],
+                                    style: Constants.dropDownMenuItemStyle,
+                                  ),
+                                  value: 0),
                               PopupMenuItem(
-                                child: Text(locations[1]),
+                                child: Text(
+                                  locations[1],
+                                  style: Constants.dropDownMenuItemStyle,
+                                ),
                                 value: 1,
                               ),
                               PopupMenuItem(
-                                child: Text(locations[2]),
+                                child: Text(
+                                  locations[2],
+                                  style: Constants.dropDownMenuItemStyle,
+                                ),
                                 value: 2,
                               ),
                             ];
                           },
                           child: Row(
                             children: [
-                              Text(locations[0]),
-                              Icon(Icons.keyboard_arrow_down)
+                              Text(locations[_selectedLocationIndex],
+                                  style: Constants.dropDownLabelStyle),
+                              Icon(Icons.keyboard_arrow_down,
+                                  color: Constants.lightColor)
                             ],
                           ),
                         ),
-                        Icon(Icons.ac_unit_outlined)
+                        Spacer(),
+                        Icon(Icons.settings, color: Constants.lightColor)
                       ],
                     ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    'Where would\nyou want to go?',
+                    style: Constants.titleStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32.0),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(Constants.bigRadius),
+                      child: TextField(
+                        controller: TextEditingController(
+                            text: locations[_selectedLocationIndex]),
+                        cursorColor: Constants.primaryColor,
+                        style: Constants.dropDownMenuItemStyle,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: Constants.defaultPadding * 2,
+                                vertical: Constants.defaultPadding),
+                            suffixIcon: Material(
+                              elevation: 2,
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(Constants.bigRadius)),
+                              child: Icon(
+                                Icons.search,
+                                color: Constants.darkColor,
+                              ),
+                            )),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            isFlightSelected = true;
+                          });
+                        },
+                        child: ChoiceChip(
+                            icon: Icons.flight_takeoff_rounded,
+                            text: 'Flights',
+                            isSelected: isFlightSelected),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            isFlightSelected = false;
+                          });
+                        },
+                        child: ChoiceChip(
+                            icon: Icons.hotel,
+                            text: 'Hotels',
+                            isSelected: !isFlightSelected),
+                      ),
+                    ],
                   )
                 ],
               ),
@@ -85,6 +190,52 @@ class _HomeScreenTopState extends State<HomeScreenTop> {
           ),
         )
       ],
+    );
+  }
+}
+
+class ChoiceChip extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  final bool isSelected;
+
+  ChoiceChip({this.icon, this.text, this.isSelected});
+
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<ChoiceChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: Constants.smallPadding,
+          horizontal: Constants.defaultPadding),
+      decoration: widget.isSelected
+          ? BoxDecoration(
+              borderRadius:
+                  BorderRadius.all(Radius.circular(Constants.bigRadius)),
+              color: Constants.lightColor.withOpacity(0.15))
+          : null,
+      child: Row(
+        // mainAxisSize: MainAxisSize.min,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            widget.icon,
+            size: 20,
+            color: Constants.lightColor,
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text(
+            widget.text,
+            style: TextStyle(fontSize: 14, color: Constants.lightColor),
+          )
+        ],
+      ),
     );
   }
 }
